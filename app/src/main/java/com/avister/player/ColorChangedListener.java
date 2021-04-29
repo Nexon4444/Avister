3c2676b88c5e702776b48399a04f42b615922237
Maciej Kutrowski <maciek44444@gmail.com>
2021-04-29 14:58:29 +0200

    Initial commit

---
+++ app/src/main/java/com/avister/player/ColorChangedListener.java
@@ -0,0 +1,7 @@
+package com.avister.player;
+
+public interface ColorChangedListener {
+    void colorChanged(int color);
+}
+
+
---
+++ app/src/main/java/com/avister/player/drawerItems/ExpandableSwitchDrawerItem.kt
@@ -0,0 +1,6 @@
+package com.avister.player.drawerItems
+
+/**
+ * Created by ditek on 09-Jul-19
+ */
+class ExpandableSwitchDrawerItem : AbstractExpandableSwitchDrawerItem<ExpandableSwitchDrawerItem>()
