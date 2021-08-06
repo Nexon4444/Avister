38d00f45bf4075a133530e362b86097d7f1e3920
Maciej Kutrowski <maciek44444@gmail.com>
2021-08-06 11:20:27 +0200

    added autcommit

--- auto_commit_push_config.sh
+++ auto_commit_push_config.sh
@@ -1,25 +1,50 @@
 #!/bin/sh
 cd .
-git branch --show-current
-temp = $?
-printf $temp
-git show-ref --verify --quiet refs/heads/BACKUP
 
-if ($? == 1)
+timestamp() {
+  date +"at %H:%M:%S on %d/%m/%Y"
+}
+temp=$(echo git branch --show-current)
+echo $temp
+#eval x=($temp == "backup")
+#echo $x
+
+if [$temp == "backup"]
 then
-  git checkout -b BACKUP
-else
+  echo "error"
+  exit -1
+fi
+echo "mowsi"
+git show-ref --verify --quiet refs/heads/backup
+
+if [$? == 0]
+then
+  printf "NOT EXISTS\n"
   git stash
-  git checkout BACKUP
+  git checkout backup
   git stash pop
   timestamp() {
     date +"at %H:%M:%S on %d/%m/%Y"
   }
-fi
-git commit -am "Regular auto-commit $(timestamp)"
 
-git checkout -b $temp
-git stash pop
+else
+  printf "EXISTS\n"
+  git stash save "backup_stash"
+  git stash branch temp-backup-branch
+#  git add .
+  git commit -am "Regular auto-commit $(timestamp)"
+  git checkout backup
+  git merge temp-backup-branch
+  git branch -D temp-backup-branch
+  git push --set-upstream origin backup
+
+  git checkout $temp
+  git stash pop "backup_stash"
+fi
+#git commit -am "Regular auto-commit $(timestamp)"
+#
+#git checkout -b $temp
+#git stash pop
 
 #  git commit -m "commit_message"
 # $? == 0 means local branch with <branch-name> exists.
