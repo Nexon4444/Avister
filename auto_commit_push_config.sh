fd87d2e8ad7b456160adc943178fdfb691b1fbcc
Maciej Kutrowski <maciek44444@gmail.com>
2021-08-06 10:17:42 +0200

    Regular auto-commit at 10:17:41 on 06/08/2021

--- auto_commit_push_config.sh
+++ auto_commit_push_config.sh
@@ -1,8 +1,34 @@
 #!/bin/sh
 cd .
-git add --all
-timestamp() {
-  date +"at %H:%M:%S on %d/%m/%Y"
-}
+git branch --show-current
+temp = $?
+printf $temp
+git show-ref --verify --quiet refs/heads/BACKUP
+
+if $? == 1
+then
+  git checkout -b BACKUP
+else
+  git stash
+  git checkout BACKUP
+  git stash pop
+  timestamp() {
+    date +"at %H:%M:%S on %d/%m/%Y"
+  }
+fi
 git commit -am "Regular auto-commit $(timestamp)"
-git push origin master
+
+git checkout -b $temp
+git stash pop
+
+#  git commit -m "commit_message"
+# $? == 0 means local branch with <branch-name> exists.
+
+#printf $?
+#git merge backup
+#git add --all
+#timestamp() {
+#  date +"at %H:%M:%S on %d/%m/%Y"
+#}
+#git commit -am "Regular auto-commit $(timestamp)"
+#git push origin master
